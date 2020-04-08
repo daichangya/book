7cb3a32e6013528a3bb602ae62992cea22b0ac79
代长亚(平台事业部) <dai.changya@xcar.com.cn>
2020-04-08 13:30:30 +0800

    init

---
+++ treeMd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#File: tree-md
+
+tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
+
+printf "# Project tree\n\n${tree}"
